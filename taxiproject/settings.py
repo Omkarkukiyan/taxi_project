fd15063e3359997758a3f0cd27473126ecc081b6
Omkar <omkarkukiyan1234@gmail.com>
2020-04-30 08:38:56 +0530

    Updated STATIC PATH

--- taxiproject/settings.py
+++ taxiproject/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'taxiproject/staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "taxiproject/static")]
 
 MEDIA_URL = '/media/'
 
